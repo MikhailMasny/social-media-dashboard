853589170bc70777279ffb6d434e4900663f6652
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-04-29 23:03:26 +0300

    init: add blank libraries and web api project

---
+++ src/SocialMediaDashboard.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SocialMediaDashboard.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/SocialMediaDashboard.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SocialMediaDashboard.Logic
+{
+    public class Class1
+    {
+    }
+}
