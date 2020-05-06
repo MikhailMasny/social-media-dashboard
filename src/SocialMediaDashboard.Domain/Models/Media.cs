8b3ca2c78a61aa779bf28543fa2d1591f14f1925
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-05-06 22:40:00 +0300

    feat: add basic (EF) models with navigation

---
+++ src/SocialMediaDashboard.Domain/Models/Media.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace SocialMediaDashboard.Domain.Models
+{
+    /// <summary>
+    /// Media model.
+    /// </summary>
+    public class Media
+    {
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Social media account.
+        /// </summary>
+        public string AccountName { get; set; }
+
+        /// <summary>
+        /// User identifier.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Navigation property for User.
+        /// </summary>
+        public User User { get; set; }
+
+        /// <summary>
+        /// Navigation to Statistic.
+        /// </summary>
+        public ICollection<Statistic> Statistics { get; set; }
+    }
+}
---
+++ src/SocialMediaDashboard.Domain/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace SocialMediaDashboard.Domain.Models
+{
+    /// <summary>
+    /// User model.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Role.
+        /// </summary>
+        public bool IsAdmin { get; set; }
+
+        /// <summary>
+        /// Navigation to Media.
+        /// </summary>
+        public ICollection<Media> Medias { get; set; }
+    }
+}
