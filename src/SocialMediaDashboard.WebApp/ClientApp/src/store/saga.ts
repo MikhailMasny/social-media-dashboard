107709dc3d2a414edeceff8833fb6cba81f8d116
Alexandr G <s207883@yandex.ru>
2020-05-07 23:04:26 +0300

    feat: add comment

--- src/SocialMediaDashboard.WebApp/ClientApp/src/store/saga.ts
+++ src/SocialMediaDashboard.WebApp/ClientApp/src/store/saga.ts
@@ -1,6 +1,10 @@
 import { all } from "redux-saga/effects";
 import AuthSaga from "../components/autorisation/saga";
 
+/**
+ * Application root saga.
+ * Register each other saga`s here.
+ */
 export default function* rootSaga() {
 	yield all([
 		...AuthSaga,
