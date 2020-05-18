d47bd781846d14cd8637cb0f51ecbdf292d3a3e1
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-05-18 18:22:57 +0300

    feat: add api routes constants class and project reference

---
+++ src/SocialMediaDashboard.Common/Constants/ApiRoutes.cs
@@ -0,0 +1,15 @@
+﻿namespace SocialMediaDashboard.Common.Constants
+{
+    /// <summary>
+    /// API route constants.
+    /// </summary>
+    public static class ApiRoutes
+    {
+        /// <summary>
+        /// Root path.
+        /// </summary>
+        public const string Root = "api";
+
+        // UNDONE: TBC
+    }
+}
